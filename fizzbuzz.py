9d24a4f7f3bc49e349c9119783b5a35d9901b5bd
Derek Chong <dc@derekchong.com>
2013-07-31 08:49:41 +0800

    Bugfix - fizzbuzz now recognises 5s

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,9 +3,11 @@ def fizzbuzz(num):
     for i in range(num):
         if (i%3==0 and i%5==0): print "FizzBuzz"
         elif i % 3 == 0: print "Fizz"
-        #elif i % 5 == 0: print "Buzz" # bug! :(
+        elif i % 5 == 0: print "Buzz"
         else: print i
 
 
 if __name__ == '__main__':
     fizzbuzz(42)
+
+    
